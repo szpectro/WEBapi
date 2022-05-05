92de5d347cf361172bd419d83aa225667952f22d
szpectro <sebastiancovarrubiasg@hotmail.com>
2022-05-05 08:44:08 -0400

    Agregar archivos de proyecto.

---
+++ WEBapi/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+using WEBapi.Models;
+
+namespace WEBapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [Authorize(Roles = "admin")]
+        [HttpGet("Admins")]
+        public IActionResult AdminsEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+        [Authorize(Roles = "Common")]
+        [HttpGet("Admins")]
+        public IActionResult CommonEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+
+        [HttpGet("Public")]
+        public IActionResult Public() 
+        {
+            return Ok("hi you're on pulbic property");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+                
+            }
+            return null;
+        }
+    }
+}
---
+++ WEBapi/Models/UserConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace WEBapi.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>() {
+            new UserModel() { Username = "admin", EmailAddress = "admin@hotmail.com", Password = "admin" , GivenName = "Admin", Surname = " Admin" , Role = "Admin" },
+        new UserModel() { Username = "admin", EmailAddress = "admin@hotmail.com", Password = "admin" , GivenName = "Admin", Surname = " Admin" , Role = "Admin" }
+
+    };
+    }
+}
